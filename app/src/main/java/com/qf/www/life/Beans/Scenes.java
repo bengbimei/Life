af58605655303e0c1f2e2c3b7700f04b1e5cb454
zxwd2015 <bravezhanggood@163.com>
2015-04-30 16:50:30 +0800

    在开始页面 添加了下载，解析数据，暂时存放数据的功能，配合跳转到MainAcivity主界面使用

--- app/src/main/java/com/qf/www/life/Beans/Scenes.java
+++ app/src/main/java/com/qf/www/life/Beans/Scenes.java
@@ -9,10 +9,45 @@ public class Scenes {
     private String sceneBackgroundImage;
     private String sceneDescription;
 
-    public Scenes(String sceneTitle, String sceneTitleImage, String sceneBackgroundImage, String sceneDescription) {
+    public String getSceneTitle() {
+        return sceneTitle;
+    }
+
+    public void setSceneTitle(String sceneTitle) {
         this.sceneTitle = sceneTitle;
+    }
+
+    public String getSceneTitleImage() {
+        return sceneTitleImage;
+    }
+
+    public void setSceneTitleImage(String sceneTitleImage) {
         this.sceneTitleImage = sceneTitleImage;
+    }
+
+    public String getSceneBackgroundImage() {
+        return sceneBackgroundImage;
+    }
+
+    public void setSceneBackgroundImage(String sceneBackgroundImage) {
         this.sceneBackgroundImage = sceneBackgroundImage;
+    }
+
+    public String getSceneDescription() {
+        return sceneDescription;
+    }
+
+    public void setSceneDescription(String sceneDescription) {
         this.sceneDescription = sceneDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "Scenes{" +
+                "sceneTitle='" + sceneTitle + '\'' +
+                ", sceneTitleImage='" + sceneTitleImage + '\'' +
+                ", sceneBackgroundImage='" + sceneBackgroundImage + '\'' +
+                ", sceneDescription='" + sceneDescription + '\'' +
+                '}';
     }
 }
--- app/src/main/java/com/qf/www/life/BeginActivity.java
+++ app/src/main/java/com/qf/www/life/BeginActivity.java
@@ -4,11 +4,19 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest;
+import com.qf.www.life.ParesUtils.BeautifulLife_parser;
 
 public class BeginActivity extends FragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_begin);
         if (isFirst())//如果第一次打开应用，就进入欢迎页面
@@ -16,7 +24,24 @@ public class BeginActivity extends FragmentActivity {
             System.out.println("-----------欢迎");
             Intent in = new Intent(this, WelActivity.class);
             startActivity(in);
+            finish();
         }
+        //下载网络数据并解析存储
+        HttpUtils httpUtils = new HttpUtils(5000);
+        httpUtils.send(HttpRequest.HttpMethod.GET, Url.beautifulLife, new RequestCallBack<String>() {
+            @Override
+            public void onSuccess(ResponseInfo<String> objectResponseInfo) {
+                if (objectResponseInfo != null) {
+                    //传人json字符串，解析数据，存储在静态集合中
+                    BeautifulLife_parser.getResult(objectResponseInfo.result);
+                }
+            }
+
+            @Override
+            public void onFailure(HttpException e, String s) {
+                Toast.makeText(BeginActivity.this, "你的网速卡爆了", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 
@@ -27,10 +52,8 @@ public class BeginActivity extends FragmentActivity {
             SharedPreferences.Editor edit = sharedPreferences.edit();
             edit.putBoolean("isFirst", false);
             edit.commit();
-            System.out.println("--------true");
             return true;
         }
-        System.out.println("--------false");
         return false;
     }
 
