a728bc6e4586968f3dc57586c7a4bb1a573db743
aaa <bengbimei07@126.com>
2015-05-02 13:49:09 +0800

    添加MainActivity中的“春季不感冒”界面

--- app/src/main/java/com/qf/www/life/MainActivity.java
+++ app/src/main/java/com/qf/www/life/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 
+import com.qf.www.life.ParesUtils.ChunjiDetail_MainActivity;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -11,7 +13,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    
+       
     }
 
 }
